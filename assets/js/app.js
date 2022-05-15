7f1631aecb162651bbd1b09b1d2192b9989f53e5
eliudkw <eliud.kihereko@student.moringaschool.com>
2022-05-15 18:08:04 +0300

    loads questions from question file

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+let currentQuestion = 0;
+let answeredCorrect = 0;
+
+// this functions loads the questions from questions file and creates a form for them in the index file
+function loadQuestion() {
+    return `<div> <fieldset>
+        <legend class="question">${questions[currentQuestion].question}</legend>
+        <input type="radio" name="exercise-question" class="exercise-question" id="answer1-1" tabindex="0" value="a">
+        <label for="answer1-1">${questions[currentQuestion].answers.a}</label>
+        <br>
+        <input type="radio" name="exercise-question" class="exercise-question" id="answer1-2" tabindex="1" value="b">
+        <label for="answer1-2">${questions[currentQuestion].answers.b}</label>
+        <br>
+        <input type="radio" name="exercise-question" class="exercise-question" id="answer1-3" tabindex="2" value="c">
+        <label for="answer1-3">${questions[currentQuestion].answers.c}</label><br>
+        <input type="radio" name="exercise-question" class="exercise-question" id="answer1-4" tabindex="3" value="d">
+        <label for="answer1-4">${questions[currentQuestion].answers.d}</label>
+        <button class = "submitAnswer" type="submit">Submit Answer</button><br>
+        <p>Question ${currentQuestion + 1} of ${questions.length}</p>
+        <p>${answeredCorrect} of ${questions.length} correct</p>
+      </fieldset>
+      </div>` ;
+}
+
