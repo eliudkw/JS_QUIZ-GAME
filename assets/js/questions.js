e67da12e07ba3346a2d7a677970478b99aa4dd10
eliudkw <eliud.kihereko@student.moringaschool.com>
2022-05-15 17:43:20 +0300

    creates questions file and adds questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,99 @@
 const questions = [
-    
+
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: {
+            a: "msg('Hello World')",
+            b: "alertBox('Hello world')",
+            c: "alert('Hello World')",
+            d: "All of the above"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "alert('Hello World')"
+    },
+
+    {
+        question: "Which of the following describes a HTML element? ",
+        answers: {
+            a: "opening_tag body closing_tag",
+            b: "opening_tag and closing_tag",
+            c: "opening_tag",
+            d: "All of the above"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "opening_tag body closing_tag"
+    },
+
+    {
+        question: "How do you declare a function in JavaScript?",
+        answers: {
+            a: "Function:functionName",
+            b: "function functionName() {//body}",
+            c: "Both a and b ",
+            d: "None of the above"
+        },
+        correctAnswer: "b",
+        correctAnswerResult: "function functionName() {//body}"
+    },
+
+    {
+        question: "How do you call a function named 'myGreetings'?",
+        answers: {
+            a: "call function myGreetings",
+            b: "call myGreetings",
+            c: "myGreetings()",
+            d: "a and b"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "myGreetings()"
+    },
+
+    {
+        question: "How to write an if statement in JavaScript?",
+        answers: {
+            a: "if condition then",
+            b: "if condition",
+            c: "if (//condition) {//body}",
+            d: "None of the above"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "if (//condition) {//body}"
+    },
+
+    {
+        question: " Which of the following is a JavaScript array?",
+        answers: {
+            a: "let color = red, blue, green ",
+            b: "let color = [ red, blue, green] ",
+            c: "let color = ' red, blue, green ' ",
+            d: "a and c"
+        },
+        correctAnswer: "b",
+        correctAnswerResult: "let color = [ red, blue, green] "
+    },
+
+    {
+        question: "What is a function?",
+        answers: {
+            a: "A function is a block of code that performs a specific task.",
+            b: "A semantic tool that is invoked when a particular action is desired",
+            c: "An action that may be performed on an object",
+            d: "A container for a data value"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "A function is a block of code that performs a specific task."
+    },
+
+    {
+        question: "How do you write a for loop statement?",
+        answers: {
+            a: "for( i=0 ; i<=5; i++) {//body}",
+            b: "for (i=1)",
+            c: "for(i =0, i< 5, i++)",
+            d: "None of above"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "for( i=0 ; i<=5; i++) {//body}"
+    },
+
 ]
